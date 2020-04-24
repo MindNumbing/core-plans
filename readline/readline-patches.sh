530ab3f88197ef4ad1e38a2b8f6f522335213510
MindNumbing <SMarshall@chef.io>
2020-04-24 14:02:16 +0100

    [readline] - Updating readline to 8.0.4
    Signed-off-by: MindNumbing <SMarshall@chef.io>

--- readline/readline-patches.sh
+++ readline/readline-patches.sh
@@ -1,7 +1,17 @@
 _patch_url_base="${_url_base}/${_distname}-${_base_version}-patches/${_distname}${_base_version//.}"
 
 # All official patch file URLs
-_patch_files=()
+_patch_files=(
+  ${_patch_url_base}-001
+  ${_patch_url_base}-002
+  ${_patch_url_base}-003
+  ${_patch_url_base}-004
+)
 
 # All official patch file shasums
-_patch_shasums=()
+_patch_shasums=(
+  d8e5e98933cf5756f862243c0601cb69d3667bb33f2c7b751fe4e40b2c3fd069
+  36b0febff1e560091ae7476026921f31b6d1dd4c918dcb7b741aa2dad1aec8f7
+  94ddb2210b71eb5389c7756865d60e343666dfb722c85892f8226b26bb3eeaef
+  b1aa3d2a40eee2dea9708229740742e649c32bb8db13535ea78f8ac15377394c
+)
